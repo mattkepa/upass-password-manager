8a1ad099c6055f6e1a5a2f7c3eebe9f4ed471ca9
Mateusz Kępa <matt.kepa@gmail.com>
2023-02-21 18:15:52 +0100

    feat: add user authentication

--- upass.py
+++ upass.py
@@ -1,5 +1,70 @@
-from utils import console, display_menu
+import bcrypt
+from getpass import getpass
+from services import connect_db
+from utils import console, display_menu, console_clear
 
+
+#
+# Connect to db
+try:
+    db = connect_db()
+    cursor = db.cursor()
+except Exception:
+    console.print('DatabaseError: connection with database failed', style='red')
+    if db is not None:
+        db.close()
+        db = None
+    exit()
+
+
+#
+# User authentication
+console_clear()
+console.print('uPass - Password Manager'.center(36), style='bold')
+print('### Unlock Database ###'.center(36))
+print('=' * 36)
+
+user = {}
+authenticated = False
+while not authenticated:
+    username = input('Username: ')
+    password = getpass('Password: ')
+
+    if len(username) == 0 or len(password) == 0:
+        console.print('Username and password fields cannot be empty.', style='red')
+        continue
+
+    try:
+        cursor.execute('SELECT * FROM users WHERE username = %s;', (username,))
+        db.commit()
+        response = cursor.fetchone()
+    except Exception:
+        console.print('Error: Something went wrong. Please try again later', style='red')
+        cursor.close()
+        db.close()
+        db = None
+        exit()
+
+    if response is not None:
+        valid_credentials = bcrypt.checkpw(bytes(password, 'utf-8'), bytes(response[2], 'utf-8'))
+        if valid_credentials:
+            user = {
+                'uid': response[0],
+                'username': response[1],
+                'password_hash': bytes(response[2], 'utf-8'),
+                'salt': bytes(response[3], 'utf-8')
+            }
+            authenticated = True
+            cursor.close()
+        else:
+            console.print('Invalid username or password.', style='red')
+    else:
+        console.print('Invalid username or password.', style='red')
+
+
+#
+# Main program loop
+console_clear() # clear console after sucessfull authentication
 while True:
     display_menu()
     choice = input('Choose an option:  ')
@@ -23,3 +88,9 @@ while True:
     else:
         console.print('Incorrect option selected', style='red')
         print()
+
+
+#
+# Close db connection
+if db is not None:
+    db.close()
